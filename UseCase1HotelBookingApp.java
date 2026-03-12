2ac88645d0d520e8c57cd67c7919300c3f528d25
NikhilNorth <ns4291@srmist.edu.in>
2026-03-12 08:47:56 +0530

    Use Case 2: Abstract Room class implemented

--- UseCase1HotelBookingApp.java
+++ UseCase1HotelBookingApp.java
@@ -1,12 +1,29 @@
+abstract class Room {
 
+    protected int numberOfBeds;
+    protected int squareFeet;
+    protected double pricePerNight;
+
+    public Room(int numberOfBeds, int squareFeet, double pricePerNight) {
+        this.numberOfBeds = numberOfBeds;
+        this.squareFeet = squareFeet;
+        this.pricePerNight = pricePerNight;
+    }
+
+    public void displayRoomDetails() {
+        System.out.println("Beds: " + numberOfBeds);
+        System.out.println("Size: " + squareFeet + " sq ft");
+        System.out.println("Price per Night: ₹" + pricePerNight);
+    }
+}
 
 public class UseCase1HotelBookingApp {
 
     public static void main(String[] args) {
 
-        System.out.println("======================================");
-        System.out.println("     Welcome to BookMyStay System     ");
-        System.out.println("======================================");
+        System.out.println("==================================");
+        System.out.println("      Welcome to BookMyStay       ");
+        System.out.println("==================================");
         System.out.println("Hotel Booking Management System Started Successfully!");
         System.out.println("System Ready.");
     }
