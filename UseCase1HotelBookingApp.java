d618b6b36c2ab6946c434a367062de615009b238
NikhilNorth <ns4291@srmist.edu.in>
2026-03-12 08:39:19 +0530

    Use Case 1: Application Entry and Welcome Message implemented

---
+++ UseCase1HotelBookingApp.java
@@ -0,0 +1,13 @@
+
+
+public class UseCase1HotelBookingApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("======================================");
+        System.out.println("     Welcome to BookMyStay System     ");
+        System.out.println("======================================");
+        System.out.println("Hotel Booking Management System Started Successfully!");
+        System.out.println("System Ready.");
+    }
+}
